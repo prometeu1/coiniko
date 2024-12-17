756e5994b9dbdf78ecf64345a647195f01d825e6
prometeu1 <kevin.bajurean@oteria.fr>
2024-12-17 13:28:13 +0100

    coinlisting v2 ( vercel error fixed with Icrypto )

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 // page.tsx
 import { fetchLatestCryptocurrencyListings } from '@/lib/coinmarketcap';
 
+interface ICrypto {
+  id: number;
+  name: string;
+  symbol: string;
+  quote: {
+    USD: {
+      price: number;
+    };
+  };
+}
+
 export default async function Page() {
   // Récupération des données côté serveur
   const cryptocurrencies = await fetchLatestCryptocurrencyListings();
@@ -9,7 +20,7 @@ export default async function Page() {
     <div>
       <h1>Liste des cryptomonnaies</h1>
       <ul>
-        {cryptocurrencies.map((crypto: any) => (
+        {cryptocurrencies.map((crypto: ICrypto) => (
           <li key={crypto.id}>
             {crypto.name} ({crypto.symbol}) - ${crypto.quote.USD.price.toFixed(2)}
           </li>
