ece1652854393ac72634f48bb94da87c2dd8c4f1
prometeu1 <kevin.bajurean@oteria.fr>
2024-12-17 10:50:36 +0100

    navbar v2 ( fix vercel error )

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,67 +7,28 @@ import { cn } from "@/lib/utils"
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
 
 export function NavigationMenuDemo() {
     return (
-      <div className="flex items-center justify-center w-full mt-8"> {/* Ajout de mt-8 pour la marge en haut */}
+      <div className="flex items-center justify-center w-full mt-8">
         <NavigationMenu>
-          <NavigationMenuList className="flex items-center justify-center space-x-6"> {/* Ajout de space-x-6 pour plus d'espace entre les éléments */}
+          <NavigationMenuList className="flex items-center justify-center space-x-6">
             <NavigationMenuItem>
               <Link href="/market-insight" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle() + " text-lg"}> {/* Augmentation de la taille et de la graisse du texte */}
+                <NavigationMenuLink className={navigationMenuTriggerStyle() + " text-lg"}>
                   Market Insights
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
               <Link href="/coinlisting" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle() + " text-lg"}> {/* Augmentation de la taille et de la graisse du texte */}
+                <NavigationMenuLink className={navigationMenuTriggerStyle() + " text-lg"}>
                   Coin Listing
                 </NavigationMenuLink>
               </Link>
